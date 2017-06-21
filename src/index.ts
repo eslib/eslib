5c9fd12b9ac1c5edb2baf7d54c27a8fb6e736fe1
Boris Cherny <boris@performancejs.com>
2017-06-21 14:39:50 +0200

    add colors

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
+import { blue, cyan, green, magenta, whiteBright } from 'cli-color'
 import { valid, satisfies, gte } from 'semver'
 
 const host = global || window
 
+/** TODO: can we make this safer by checking that `name` is a key on `type.proto`? */
 export function assign(
   name: string,
   fn: Function,
@@ -20,7 +22,7 @@ export function assign(
 
   // validate version
   if (!valid(version)) {
-    console.warn(`version string "${version}" for method "${name}" on type "${type}" is invalid. Please specify version as X.Y.Z (eg. "1.2.3").`)
+    warn(`Version string ${magenta(version)} for method ${green(name)} on type ${blue(type)} is invalid - please specify version as X.Y.Z (eg. ${magenta('1.2.3')})`)
     return
   }
 
@@ -30,24 +32,24 @@ export function assign(
 
     // if method is already natively defined, skip it
     if (isNative(existing)) {
-      console.warn(`ESLib warning: skipping method "${name}" because it is already natively installed on type "${type}".`)
+      warn(`Skipping method ${green(name)} because it is already natively installed on ${blue(type)}`)
       return
     }
 
     // if property is defined by something else, skip it
     if (!('source' in existing) || !('version' in existing) || !('originalFunction' in existing)) {
-      console.warn(`ESLib warning: skipping method "${name}" because it is already defined on type "${type}" by some library outside of ESlib.`)
+      warn(`Skipping method ${green(name)} because it is already defined on ${blue(type)} by some library outside of ESlib`)
     }
 
     // if method is defined by another eslib, skip it
     if (existing.source !== _fn.source) {
-      console.warn(`ESLib warning: skipping method "${name}" (provided by "${_fn.source}") on type "${type}" because another method with the same name was already installed by "${existing.source}."`)
+      warn(`Skipping method ${green(name)} (provided by ${cyan(_fn.source)}) on ${blue(type)} because another method with the same name was already installed by ${cyan(existing.source)}`)
       return
     }
 
     // if method is defined by the same eslib at an incompatible version, skip it
     if (!isCompatible(existing, _fn)) {
-      console.warn(`ESLib warning: skipping method "${name}" at version ${_fn.version} (provided by "${_fn.source}") because a${gte(existing.version, _fn.version) ? ' newer' : 'n older'} version ${existing.version} is already installed on type "${type}".`)
+      warn(`Skipping method ${green(name)} at version ${magenta(_fn.version)} (provided by ${cyan(_fn.source)}) because a${gte(existing.version, _fn.version) ? ' newer' : 'n older'} version ${magenta(existing.version)} is already installed on ${blue(type)}.`)
       return
     }
   }
@@ -76,4 +78,8 @@ function isCompatible(a: Extension, b: Extension) {
 
 function isNative(fn: Function) {
   return fn.toString().indexOf('[native code]') > -1
+}
+
+function warn(message: string) {
+  console.warn(whiteBright.bgRed(`ESLib warning`), message)
 }
